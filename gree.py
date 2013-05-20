4bb0afd2bc268d562389754ae29cf0879db837fb
dutchensong <dutchensong@gmail.com>
2013-05-20 23:54:19 +0800

    add script

---
+++ gree.py
@@ -0,0 +1,191 @@
+import sys
+
+DATE = sys.argv[1]
+
+f_install = open("install/"+DATE,"r")
+f_click = open("click/"+DATE,"r")
+
+setting_list = []
+
+data_dict={}
+
+Setting_dict={
+	1544:"Book Of Ashes",
+	1545:"Kindom Age",
+	1546:"Knights",  
+	1547:"NFL Shuffle",
+	1548:"JackpotSlots",
+	1549:"MLB",
+	2195:"Crime City!",
+	2506:"Knights",
+	2508:"Saga",
+	2543:"Modern War",
+	2544:"test111",
+	2545:"Modern War !"
+}
+
+# dict = 
+# {
+# 	2195:
+# 	{
+# 		"PHONE":
+# 		{
+# 			"install":0,
+# 			"impression":0,
+# 			"click":0,
+# 			"spend":0
+# 		},
+# 			"PAD":
+# 		{
+# 			"install":0,
+# 			"impression":0,
+# 			"click":0,
+# 			"spend":0
+# 		}
+# 	}
+# }
+
+# 2013-04-27	1729	2195	PHONE	90
+
+def handle_install(points,date,device_type,SETTING):
+	if points>0 and date==DATE:
+		SETTING_NAME=Setting_dict[SETTING]
+		if device_type in ["PHONE","PAD"]:
+			device_type_translate = "Android"+device_type
+		else:
+			device_type_translate = device_type
+		
+		if SETTING_NAME in data_dict:
+
+			data_dict[SETTING_NAME][device_type_translate]["install"] = data_dict[SETTING_NAME][device_type_translate]["install"]+ 1
+			data_dict[SETTING_NAME][device_type_translate]["spend"] = data_dict[SETTING_NAME][device_type_translate]["spend"]+ points
+		else:
+			if device_type in ["PHONE","PAD"]:
+				#print 'create '+ str(SETTING)+' from handle_install'
+				data_dict[SETTING_NAME]={
+					"AndroidPHONE":
+					{
+						"install":0,
+						"impression":0,
+						"click":0,
+						"spend":0
+					},
+					"AndroidPAD":
+					{
+						"install":0,
+						"impression":0,
+						"click":0,
+						"spend":0
+					}
+				}
+			else:
+				data_dict[SETTING_NAME]={
+					"iPHONE":
+					{
+						"install":0,
+						"impression":0,
+						"click":0,
+						"spend":0
+					},
+					"iPAD":
+					{
+						"install":0,
+						"impression":0,
+						"click":0,
+						"spend":0
+					}
+				}
+			data_dict[SETTING_NAME][device_type_translate]["install"] = data_dict[SETTING_NAME][device_type_translate]["install"]+ 1
+			data_dict[SETTING_NAME][device_type_translate]["spend"] = data_dict[SETTING_NAME][device_type_translate]["spend"]+ points
+
+
+def handle_impression_and_click(date,event_type,sid,device_type):
+	if date == DATE:
+		SETTING_NAME=Setting_dict[sid]
+		if device_type in ["PHONE","PAD"]:
+			device_type_translate = "Android"+device_type
+		else:
+			device_type_translate = device_type
+		if SETTING_NAME in data_dict:
+			#print event_type
+			if event_type == '-3':
+				data_dict[SETTING_NAME][device_type_translate]["impression"] = data_dict[SETTING_NAME][device_type_translate]["impression"] + 1
+			elif event_type == '-2':
+				data_dict[SETTING_NAME][device_type_translate]["click"] = data_dict[SETTING_NAME][device_type_translate]["click"] + 1
+		else:
+			#print sid + ' is not in data_dict'
+			if device_type in ["PHONE","PAD"]:
+				data_dict[SETTING_NAME]={
+					"AndroidPHONE":
+					{
+						"install":0,
+						"impression":0,
+						"click":0,
+						"spend":0
+					},
+					"AndroidPAD":
+					{
+						"install":0,
+						"impression":0,
+						"click":0,
+						"spend":0
+					}
+				}
+			else:
+				data_dict[SETTING_NAME]={
+					"iPHONE":
+					{
+						"install":0,
+						"impression":0,
+						"click":0,
+						"spend":0
+					},
+					"iPAD":
+					{
+						"install":0,
+						"impression":0,
+						"click":0,
+						"spend":0
+					}
+				}
+			
+
+				
+# handle install file
+for line in f_install:
+	items = line[:-1].split("\t")
+
+	sid = int(items[2])
+	points = int(items[4])
+	date = items[0]
+	device_type = items[3]
+
+	handle_install(points,date,device_type,sid)
+
+
+# handle impression and click file
+for line_ic in f_click:
+	items = line_ic[:-1].split("\t")
+
+	date = items[0]
+	sid = int(items[3])
+	event_type = items[1]
+	device_type = items[4]
+
+	handle_impression_and_click(date,event_type,sid,device_type)
+
+
+
+
+g=open('Gree-'+DATE+'.csv','a')
+print >>g, "date,sid,device_type,impression,click,install,points,spend($)"
+for setting in data_dict:
+	for device_type in data_dict[setting]:
+		#print setting+','+device_type
+		print >>g, DATE+','+str(setting)+','+device_type+','+str(data_dict[setting][device_type]['impression'])+','+str(data_dict[setting][device_type]['click'])+','+str(data_dict[setting][device_type]['install'])+','+str(data_dict[setting][device_type]['spend'])+','+str(int(data_dict[setting][device_type]['spend'])/100)
+#        print g
+#print data_dict
+
+
+f_install.close()
+f_click.close()
